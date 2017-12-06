794947c39a2bb900d619c700074dc7da44878d93
Jiajie Mai <33970456+Jiajie-Mai@users.noreply.github.com>
2017-12-05 21:41:53 -0500

    Update Assassin.java
    Changed the critChance variable and added some informative comments.

--- v2/Assassin.java
+++ v2/Assassin.java
@@ -1,6 +1,9 @@
+//Unsure on how to use super(); completely
 public class Assassin extends Protagonist{
+	//crit variable
     private double critChance;
 
+	//amplifies damage if Math.random() is lower than the crit chance
     private int dmgAmplify() {
 	int dmgAmplify = 0;
 	if (critChance <= Math.random()) {
@@ -10,17 +13,20 @@ public class Assassin extends Protagonist{
 	return dmgAmplify;
     }
     
+	//Assassin constructor
     public Assassin(String aName) {
 	super(aName);
-	critChance = 0.1;
+	critChance = 0.5;
     }
 
+	//critAttack() gives the chance for an empowered attack
     public int critAttack(Character npc) {
 	int damage = (int) (strength * atk * (dmgAmplify())) - npc.getDefense();
 	npc.lowerHP(damage);
 	return damage;
     }
 
+	//toString for new class
     public String tostring() {
 	return "The assassin backstabbed its target for " + super.toString();
     }
